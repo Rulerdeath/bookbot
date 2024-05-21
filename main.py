205a787a819efdfe3d56413803574998e0e336e3
Rulerdeath <alexcajina77@gmail.com>
2024-05-20 22:51:22 -0600

    Added function to sort the letters by the amount of times they appear

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 def main():
+    print("-- Begin report of Frankenstein --")
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         dict_list=[]
-        print(f"There are {count_words(file_contents)} words")
-        print (count_letters(file_contents))
-        print(file_contents)
+        print(f"There are {count_words(file_contents)} words"), print()
+        sort_letters = count_letters(file_contents)
+        sort_times(sort_letters)
+        print("-- End of report --")
 
 def count_words(file_contents):
     words=file_contents.split()
@@ -18,11 +20,17 @@ def count_letters(file_contents):
     letter_count = {}
     lwrcase_txt = file_contents.lower()
     for letter in lwrcase_txt:
-        if letter in letter_count:
-            letter_count[letter] +=1
-        else:
-            letter_count[letter] = 1
+        if letter.isalpha():
+                if letter in letter_count:
+                    letter_count[letter] +=1
+                else:
+                    letter_count[letter] = 1
     return letter_count
 
+def sort_times(sort_letters):
+    sort_letters = list(sort_letters.items()) # Turns the characters' dictionary into a list of tuples
+    sort_letters.sort(key=lambda tuple: tuple[1], reverse=True) # Sorts the list by their amount of occurrences
+    for tuple in sort_letters:
+        print(f"The character {tuple[0]} was found {tuple[1]} times")
 
 main()
